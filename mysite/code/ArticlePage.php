6fad061b1f05073dd019286363adc5090abd5597
midora64 <midora64@gmail.com>
2017-05-15 09:05:51 +1200

    lesson 7

--- mysite/code/ArticlePage.php
+++ mysite/code/ArticlePage.php
@@ -7,12 +7,30 @@ class ArticlePage extends Page {
 			'Author' => 'Varchar'
 	);
 	
+	private static $has_one = array(
+			'Photo' => 'Image',
+			'Brochure' => 'File'
+	);
+	
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
 		$fields->addFieldToTab('Root.Main', DateField::create('Date', 'Date of article')
 												->setConfig('showcalendar', true), 'Content');
 		$fields->addFieldToTab('Root.Main', TextareaField::create('Teaser'), 'Content');
 		$fields->addFieldToTab('Root.Main', TextField::create('Author', 'Author or article'), 'Content');
+		
+		$fields->addFieldToTab('Root.Attachments', $photo = UploadField::create('Photo'));
+		$fields->addFieldToTab('Root.Attachments', $brochure = UploadField::create(
+				'Brochure',
+				'Travel brochure, optional (PDF only)'
+				));
+		$photo->setFolderName('travel-photos');
+		$photo->getValidator()->setAllowedExtensions(array(
+				'jpg', 'jpeg', 'png', 'gif'
+		));
+		$brochure->setFolderName('travel-brochures');
+		$brochure->getValidator()->setAllowedExtensions(array('pdf'));
+		
 		return $fields;
 	}
 }
