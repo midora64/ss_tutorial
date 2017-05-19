32f0a479d5c1982e883311ca0cb0bb2c6a1d9463
midora64 <midora64@gmail.com>
2017-05-20 08:10:46 +1200

    lesson 8

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -5,4 +5,9 @@ class HomePage extends Page {
 
 class HomePage_Controller extends Page_Controller {
 	
+	public function LatestArticles($count = 3){
+		return ArticlePage::get()
+				->sort('Created', 'DESC')
+				->limit($count);
+	}
 }
